c0e594d37d4eeee10db8b5fc13db2f0e25242991
Jose Ruivo <jruivo.dev@gmail.com>
2017-09-08 19:05:26 +0100

    Every operator button seem to be working as intened.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,65 @@
-let calculator = {
-    screen: 0,
-    userInput: [],
-    stack: 0,
+// let calculator = {
 
-    init() {
-        screen: 0,
-        userInput = ['0'];
-        stack = 0;
-    },
 
-    numberToScreen(button, screen) {
+//     init() {
+//         screen: null,
+//         userInput = [];
+//         stack = null;
+//         operation: null;
+//         stackOperation: null;
+//     },
 
-    }
-}
+//     numberToScreen(button, screen) {
+
+//     }
+// }
 
 
 
 let numberButtons = document.querySelectorAll('div.number');
 let calculatorScreenEle = document.querySelector('.calculator-input');
+let operatorsEle = document.querySelectorAll('div.operator');
+let equalsEle = document.querySelector('div.equals');
 
-numberButtons.forEach(button => {
-    button.addEventListener('mouseup', getUserInput)
-});
 
-function getUserInput(e) {
-    if (userInput.length >= 10)
+let screen = null;
+let userInput = [];
+let stack = null;
+let operationInput = null;
+let stackOperation = null;
+
+function init() {
+    screen: null,
+    userInput = [];
+    stack = null;
+    operation: null;
+    stackOperation: null;
+};
+
+
+function setHandlers() {
+    numberButtons.forEach(button => {
+        button.addEventListener('mouseup', numbersHandler)
+    });
+
+    operatorsEle.forEach(button => {
+        button.addEventListener('mouseup', operatorHandler)
+    });
+
+    equalsEle.addEventListener('mouseup', equalsHandler);
+}
+
+function numbersHandler(e) {
+    let num = e.target.textContent;
+
+    if (userInput.length >= 10) //limit screen characters
         return;
-    let num = (e.target.textContent);
 
     if (userInput[0] == 0 && userInput.length == 1) {
         if (num == 0) //ignores 0 input if it's the first number
             return;
 
-        if (num != '.') { // change 0 to num if num!= 0
+        if (num != '.') { // changes 0 to num if num!= 0
             userInput = [];
         }
     }
@@ -42,18 +69,90 @@ function getUserInput(e) {
         return;
 
     userInput.push(num);
-    console.log(userInput);
-
     updateScreen();
 }
 
+function operatorHandler(e) {
+    operationInput = e.target.textContent;
+    let result;
 
-function updateScreen() {
-    screen.innerHTML = "";
-    screen = userInput.join('');
-    calculatorScreenEle.textContent = screen;
+    //detects 'C' cleaner
+    if (operationInput == 'C') {
+        init();
+        updateScreen();
+        return;
+    }
+
+    //Changes X to *
+    if (operationInput == 'X')
+        currentOperation = '*';
+    else
+        currentOperation = operationInput;
+
+    // if screen is empty , do nothing
+    if (screen == null || screen.length == 0)
+        return;
+
+    else {
+        if (stack != null) {
+            result = doMath(stack, screen, stackOperation);
+            stack = result;
+            userInput = [];
+            updateScreen(result)
+        } else {
+            stack = screen;
+            stackOperation = currentOperation;
+            userInput = [];
+            updateScreen('');
+        }
+        if (stackOperation != null) {
+            result = doMath(stack, screen, stackOperation);
+            stackOperation = currentOperation;
+        }
+    }
 }
 
 
+function equalsHandler() {
+    if (stack == null) return; // prevents 'bug' clicking equals twice
 
-calculator.init();
+    let result = doMath(stack, screen, currentOperation);
+    stack = null;
+    stackOperation = null;
+    updateScreen(result);
+}
+
+function doMath(a, b, operator) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch (operator) {
+        case '+':
+            return a + b;
+            break;
+        case '-':
+            return a - b;
+            break;
+        case '*':
+            return a * b;
+            break;
+        case '/':
+            return a / b;
+            break;
+    }
+
+}
+
+function updateScreen(result) {
+
+    if (arguments.length > 0)
+        screen = result;
+    else
+        screen = userInput.join('');
+
+    calculatorScreenEle.textContent = screen;
+    console.log(`Screen ${screen} | userInput ${userInput} | stack: ${stack} `);
+    console.log(`stackOperation ${stackOperation} | operationInput ${operationInput} `);
+}
+
+setHandlers();
+init();
